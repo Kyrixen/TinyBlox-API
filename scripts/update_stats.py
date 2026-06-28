2e701e2167a31eb9b2794040040d50e946278004
Kyrixen <kyrixen@atomicmail.io>
2026-06-28 11:32:41 +0200

    Updated stats to use itch.io official api

--- scripts/update_stats.py
+++ scripts/update_stats.py
@@ -1,51 +1,21 @@
 import json
 import os
 
-from google.analytics.data_v1beta import BetaAnalyticsDataClient
-from google.analytics.data_v1beta.types import (
-    RunReportRequest,
-    DateRange,
-    Dimension,
-    Metric,
-)
+import requests
 
-PROPERTY_ID = "543461935"
+api_key = os.environ["ITCH_API_KEY"]
 
-client = BetaAnalyticsDataClient()
+response = requests.get(f"https://itch.io/api/1/{api_key}/my-games")
+response.raise_for_status()
 
-# Total views in last 30 days
-request = RunReportRequest(
-    property=f"properties/{PROPERTY_ID}",
-    dimensions=[Dimension(name="date")],
-    metrics=[Metric(name="screenPageViews")],
-    date_ranges=[DateRange(start_date="2026-06-25", end_date="today")],
-)
-
-response = client.run_report(request)
-
-views = 0
-
-for row in response.rows:
-    views += int(row.metric_values[0].value)
-
-request = RunReportRequest(
-    property=f"properties/{PROPERTY_ID}",
-    dimensions=[Dimension(name="country")],
-    metrics=[Metric(name="screenPageViews")],
-    date_ranges=[DateRange(start_date="2026-06-25", end_date="today")],
-)
-
-response = client.run_report(request)
-topCountry = response.rows[0].dimension_values[0].value
-
+game = response.json()["games"][0]
 
 stats = {
-    "views": views,
-    "topCountry" : topCountry
+    "views": game["views_count"],
+    "downloads": game["downloads_count"]
 }
 
 path = "api/stats.json"
-
 if os.path.exists(path):
     with open(path, "r") as f:
         old_stats = json.load(f)
